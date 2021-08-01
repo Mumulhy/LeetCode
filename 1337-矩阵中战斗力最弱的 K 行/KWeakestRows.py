fe823d355a28b13f0006168b10e5981f252d3701
_Mumu <1043994188@qq.com>
2021-08-01 21:22:09 +0800

    2021/08/01 21:21
    通过1337

---
+++ 1337-矩阵中战斗力最弱的 K 行/KWeakestRows.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# LeetCode 1337-矩阵中战斗力最弱的 K 行
+
+"""
+Created on Sun Aug 1 20:19 2021
+
+@author: _Mumu
+Environment: py38
+"""
+
+from typing import List
+
+
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        from bisect import bisect_left
+        n = len(mat[0])
+        strengths = [(n - bisect_left(solders[::-1], 1)) * 1000 + i for i, solders in enumerate(mat)]
+        return [strength % 1000 for strength in sorted(strengths)][:k]
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.kWeakestRows(mat=[[1, 1, 0, 0, 0],
+                              [1, 1, 1, 1, 0],
+                              [1, 0, 0, 0, 0],
+                              [1, 1, 0, 0, 0],
+                              [1, 1, 1, 1, 1]],
+                         k=3))
