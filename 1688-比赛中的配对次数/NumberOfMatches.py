a1047c893e309d12bd90abebba755cdd854fdb25
_Mumu <49722959+Mumulhy@users.noreply.github.com>
2022-01-25 15:31:54 +0800

    2022/01/25 15:31
    通过1688

---
+++ 1688-比赛中的配对次数/NumberOfMatches.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# LeetCode 1688-比赛中的配对次数
+
+"""
+Created on Tues Jan 25 15:15 2022
+
+@author: _Mumu
+Environment: py38
+"""
+
+
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        return n - 1
