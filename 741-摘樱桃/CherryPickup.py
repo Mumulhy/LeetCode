b72a633bbafd0e47d59b2ff3dba13c2524449d12
_Mumu <49722959+Mumulhy@users.noreply.github.com>
2022-07-10 11:22:06 +0800

    2022/07/10 11:21
    通过741

--- 741-摘樱桃/CherryPickup.py
+++ 741-摘樱桃/CherryPickup.py
@@ -10,6 +10,7 @@ Environment: py38
 
 from typing import List
 
+
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
         n = len(grid)
@@ -28,15 +29,16 @@ class Solution:
                     if x2:
                         res = max(res, f[x1][x2 - 1])
                     if x1 and x2:
-                        res = max(res, f[x1 - 1][x2 - 2])
+                        res = max(res, f[x1 - 1][x2 - 1])
                     res += grid[x1][y1]
                     if x2 != x1:
                         res += grid[x2][y2]
                     f[x1][x2] = res
         return max(f[-1][-1], 0)
 
+
 if __name__ == '__main__':
     s = Solution()
     print(s.cherryPickup([[0, 1, -1],
- [1, 0, -1],
- [1, 1,  1]]))
+                          [1, 0, -1],
+                          [1, 1, 1]]))
