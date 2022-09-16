8e777ebccab53523cb3ab0903693caed64829d48
_Mumu <16307130121@fudan.edu.cn>
2022-09-16 09:47:53 +0800

    2022/09/16 09:47
    通过850

---
+++ 850-矩形面积II/RectangleArea.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+# LeetCode 850-矩形面积II
+
+"""
+Created on Fri Sept 16 09:42 2022
+
+@author: _Mumu
+Environment: py39
+"""
+
+from bisect import bisect_left
+from typing import List
+
+
+class Segtree:
+    def __init__(self):
+        self.cover = 0
+        self.length = 0
+        self.max_length = 0
+
+
+class Solution:
+    def rectangleArea(self, rectangles: List[List[int]]) -> int:
+        hbound = set()
+        for rect in rectangles:
+            # 下边界
+            hbound.add(rect[1])
+            # 上边界
+            hbound.add(rect[3])
+
+        hbound = sorted(hbound)
+        m = len(hbound)
+        # 线段树有 m-1 个叶子节点，对应着 m-1 个会被完整覆盖的线段，需要开辟 ~4m 大小的空间
+        tree = [Segtree() for _ in range(m * 4 + 1)]
+
+        def init(idx: int, l: int, r: int) -> None:
+            tree[idx].cover = tree[idx].length = 0
+            if l == r:
+                tree[idx].max_length = hbound[l] - hbound[l - 1]
+                return
+
+            mid = (l + r) // 2
+            init(idx * 2, l, mid)
+            init(idx * 2 + 1, mid + 1, r)
+            tree[idx].max_length = tree[idx * 2].max_length + tree[idx * 2 + 1].max_length
+
+        def update(idx: int, l: int, r: int, ul: int, ur: int, diff: int) -> None:
+            if l > ur or r < ul:
+                return
+            if ul <= l and r <= ur:
+                tree[idx].cover += diff
+                pushup(idx, l, r)
+                return
+
+            mid = (l + r) // 2
+            update(idx * 2, l, mid, ul, ur, diff)
+            update(idx * 2 + 1, mid + 1, r, ul, ur, diff)
+            pushup(idx, l, r)
+
+        def pushup(idx: int, l: int, r: int) -> None:
+            if tree[idx].cover > 0:
+                tree[idx].length = tree[idx].max_length
+            elif l == r:
+                tree[idx].length = 0
+            else:
+                tree[idx].length = tree[idx * 2].length + tree[idx * 2 + 1].length
+
+        init(1, 1, m - 1)
+
+        sweep = list()
+        for i, rect in enumerate(rectangles):
+            # 左边界
+            sweep.append((rect[0], i, 1))
+            # 右边界
+            sweep.append((rect[2], i, -1))
+        sweep.sort()
+
+        ans = i = 0
+        while i < len(sweep):
+            j = i
+            while j + 1 < len(sweep) and sweep[i][0] == sweep[j + 1][0]:
+                j += 1
+            if j + 1 == len(sweep):
+                break
+
+            # 一次性地处理掉一批横坐标相同的左右边界
+            for k in range(i, j + 1):
+                _, idx, diff = sweep[k]
+                # 使用二分查找得到完整覆盖的线段的编号范围
+                left = bisect_left(hbound, rectangles[idx][1]) + 1
+                right = bisect_left(hbound, rectangles[idx][3])
+                update(1, 1, m - 1, left, right, diff)
+
+            ans += tree[1].length * (sweep[j + 1][0] - sweep[j][0])
+            i = j + 1
+
+        return ans % (10 ** 9 + 7)
